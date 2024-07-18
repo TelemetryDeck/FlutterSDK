30c986157ad18a5f1fa06150ca5a800ee5015a91
Konstantin <konstantin@headbright.be>
2024-07-18 12:15:07 +0200

    chore: Migrate to Kotlin style for code formatting

--- android/src/main/kotlin/com/telemetrydeck/telemetrydecksdk/TelemetrydecksdkPlugin.kt
+++ android/src/main/kotlin/com/telemetrydeck/telemetrydecksdk/TelemetrydecksdkPlugin.kt
@@ -17,176 +17,188 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 /** TelemetrydecksdkPlugin */
-class TelemetrydecksdkPlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
-  private var applicationContext: Context? = null
-  private val coroutineScope = CoroutineScope(Dispatchers.IO) // Coroutine scope for background tasks
+class TelemetrydecksdkPlugin : FlutterPlugin, MethodCallHandler {
+    /// The MethodChannel that will the communication between Flutter and native Android
+    ///
+    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+    /// when the Flutter Engine is detached from the Activity
+    private lateinit var channel: MethodChannel
+    private var applicationContext: Context? = null
+    private val coroutineScope =
+        CoroutineScope(Dispatchers.IO) // Coroutine scope for background tasks
 
-  override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    applicationContext = flutterPluginBinding.applicationContext
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "telemetrydecksdk")
-    channel.setMethodCallHandler(this)
-  }
-
-  override fun onMethodCall(call: MethodCall, result: Result) {
-    if (call.method == "start") {
-      nativeInitialize(call, result)
-    } else if (call.method == "stop") {
-      nativeStop(result)
-    } else if (call.method == "send") {
-      // this maps to the queue method which aligns with the behaviour of the iOS SDK
-      nativeQueue(call, result)
-    } else if (call.method == "generateNewSession") {
-      TelemetryManager.newSession()
-      result.success(null)
-    } else if (call.method == "updateDefaultUser") {
-      nativeUpdateDefaultUser(call, result)
-    }else if (call.method == "navigate") {
-      nativeNavigate(call, result)
-    }else if (call.method == "navigateToDestination") {
-      nativeNavigateDestination(call, result)
-    } else {
-      result.notImplemented()
-    }
-  }
-
-  /**
-   * Send a signal that represents a navigation event with a source and a destination.
-   *
-   * @see <a href="https://telemetrydeck.com/docs/articles/navigation-signals/">Navigation Signals</a>
-   * */
-  private fun nativeNavigate(call: MethodCall, result: Result) {
-    val sourcePath = call.argument<String>("sourcePath")
-    val destinationPath = call.argument<String>("destinationPath")
-    val clientUser = call.argument<String?>("clientUser")
-
-    if (sourcePath == null || destinationPath == null) {
-      result.error("INVALID_ARGUMENT", "sourcePath and destinationPath are required", null)
-      return
+    override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        applicationContext = flutterPluginBinding.applicationContext
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "telemetrydecksdk")
+        channel.setMethodCallHandler(this)
     }
 
-    coroutineScope.launch {
-      TelemetryManager.navigate(sourcePath, destinationPath, clientUser)
-      withContext(Dispatchers.Main) {
-        result.success(null)
+    override fun onMethodCall(call: MethodCall, result: Result) {
+      when (call.method) {
+          "start" -> {
+              nativeInitialize(call, result)
+          }
+          "stop" -> {
+              nativeStop(result)
+          }
+          "send" -> {
+              // this maps to the queue method which aligns with the behaviour of the iOS SDK
+              nativeQueue(call, result)
+          }
+          "generateNewSession" -> {
+              TelemetryManager.newSession()
+              result.success(null)
+          }
+          "updateDefaultUser" -> {
+              nativeUpdateDefaultUser(call, result)
+          }
+          "navigate" -> {
+              nativeNavigate(call, result)
+          }
+          "navigateToDestination" -> {
+              nativeNavigateDestination(call, result)
+          }
+          else -> {
+              result.notImplemented()
+          }
       }
     }
-  }
 
-  /**
-   * Send a signal that represents a navigation event with a destination and a default source.
-   *
-   * @see <a href="https://telemetrydeck.com/docs/articles/navigation-signals/">Navigation Signals</a>
-   * */
-  private fun nativeNavigateDestination(call: MethodCall, result: Result) {
-    val destinationPath = call.argument<String>("destinationPath")
-    val clientUser = call.argument<String?>("clientUser")
+    /**
+     * Send a signal that represents a navigation event with a source and a destination.
+     *
+     * @see <a href="https://telemetrydeck.com/docs/articles/navigation-signals/">Navigation Signals</a>
+     * */
+    private fun nativeNavigate(call: MethodCall, result: Result) {
+        val sourcePath = call.argument<String>("sourcePath")
+        val destinationPath = call.argument<String>("destinationPath")
+        val clientUser = call.argument<String?>("clientUser")
 
-    if (destinationPath == null) {
-      result.error("INVALID_ARGUMENT", "destinationPath is required", null)
-      return
-    }
-
-    coroutineScope.launch {
-      TelemetryManager.navigate(destinationPath, clientUser)
-      withContext(Dispatchers.Main) {
-        result.success(null)
-      }
-    }
-  }
-
-  private fun nativeStop(result: Result) {
-    coroutineScope.launch {
-      TelemetryManager.stop()
-      withContext(Dispatchers.Main) {
-        result.success(null)
-      }
-    }
-  }
-
-  private fun nativeUpdateDefaultUser(call: MethodCall,
-                                       result: Result) {
-    val user = call.arguments<String>()
-
-    coroutineScope.launch {
-      TelemetryManager.newDefaultUser(user)
-      withContext(Dispatchers.Main) {
-        result.success(null)
-      }
-    }
-  }
-
-  private fun nativeQueue(
-      call: MethodCall,
-      result: Result
-  ) {
-    val signalType = call.argument<String>("signalType")
-    if (signalType != null) {
-      val clientUser = call.argument<String?>("clientUser")
-      val additionalPayload = call.argument<Map<String, String>>("additionalPayload")
-
-      coroutineScope.launch {
-        TelemetryManager.queue(signalType, clientUser, additionalPayload.orEmpty())
-
-        withContext(Dispatchers.Main) {
-          result.success(null)
+        if (sourcePath == null || destinationPath == null) {
+            result.error("INVALID_ARGUMENT", "sourcePath and destinationPath are required", null)
+            return
         }
-      }
-    } else {
-      result.error("INVALID_ARGUMENT", "signalType must be provided", null)
+
+        coroutineScope.launch {
+            TelemetryManager.navigate(sourcePath, destinationPath, clientUser)
+            withContext(Dispatchers.Main) {
+                result.success(null)
+            }
+        }
     }
-  }
 
-  private fun nativeInitialize(call: MethodCall, result: Result) {
-    val arguments = call.arguments as? Map<*, *> // Cast to a Map
-    if (arguments != null) {
-      // Extract values using the expected keys
-      // we extract the required appID parameter
-      val appID = arguments["appID"] as? String
-      if (appID == null) {
-        result.error("INVALID_ARGUMENT", "Expected value appID is not provided.", null)
-        return
-      }
+    /**
+     * Send a signal that represents a navigation event with a destination and a default source.
+     *
+     * @see <a href="https://telemetrydeck.com/docs/articles/navigation-signals/">Navigation Signals</a>
+     * */
+    private fun nativeNavigateDestination(call: MethodCall, result: Result) {
+        val destinationPath = call.argument<String>("destinationPath")
+        val clientUser = call.argument<String?>("clientUser")
 
-      // additional optional parameters
-      val apiBaseURL = arguments["apiBaseURL"] as? String?
-      val defaultUser = arguments["defaultUser"] as? String?
-      val debug = arguments["debug"] as? Boolean
-      val testMode = arguments["testMode"] as? Boolean
+        if (destinationPath == null) {
+            result.error("INVALID_ARGUMENT", "destinationPath is required", null)
+            return
+        }
 
-
-      // Initialize the client
-      // Do not activate the lifecycle provider
-      val builder = TelemetryManager.Builder()
-        .appID(appID)
-        .providers(listOf(SessionProvider(), EnvironmentMetadataProvider()))
-
-      apiBaseURL?.let {
-        builder.baseURL(it)
-      }
-      defaultUser?.let {
-        builder.defaultUser(it)
-      }
-      debug?.let {
-        builder.showDebugLogs(it)
-      }
-      testMode?.let {
-        builder.testMode(it)
-      }
-
-      val application = applicationContext as Application
-      TelemetryManager.start(application, builder)
-      result.success(null)
-    } else {
-      result.error("INVALID_ARGUMENT", "Arguments are not a map", null)
+        coroutineScope.launch {
+            TelemetryManager.navigate(destinationPath, clientUser)
+            withContext(Dispatchers.Main) {
+                result.success(null)
+            }
+        }
     }
-  }
 
-  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
+    private fun nativeStop(result: Result) {
+        coroutineScope.launch {
+            TelemetryManager.stop()
+            withContext(Dispatchers.Main) {
+                result.success(null)
+            }
+        }
+    }
+
+    private fun nativeUpdateDefaultUser(
+        call: MethodCall,
+        result: Result
+    ) {
+        val user = call.arguments<String>()
+
+        coroutineScope.launch {
+            TelemetryManager.newDefaultUser(user)
+            withContext(Dispatchers.Main) {
+                result.success(null)
+            }
+        }
+    }
+
+    private fun nativeQueue(
+        call: MethodCall,
+        result: Result
+    ) {
+        val signalType = call.argument<String>("signalType")
+        if (signalType != null) {
+            val clientUser = call.argument<String?>("clientUser")
+            val additionalPayload = call.argument<Map<String, String>>("additionalPayload")
+
+            coroutineScope.launch {
+                TelemetryManager.queue(signalType, clientUser, additionalPayload.orEmpty())
+
+                withContext(Dispatchers.Main) {
+                    result.success(null)
+                }
+            }
+        } else {
+            result.error("INVALID_ARGUMENT", "signalType must be provided", null)
+        }
+    }
+
+    private fun nativeInitialize(call: MethodCall, result: Result) {
+        val arguments = call.arguments as? Map<*, *> // Cast to a Map
+        if (arguments != null) {
+            // Extract values using the expected keys
+            // we extract the required appID parameter
+            val appID = arguments["appID"] as? String
+            if (appID == null) {
+                result.error("INVALID_ARGUMENT", "Expected value appID is not provided.", null)
+                return
+            }
+
+            // additional optional parameters
+            val apiBaseURL = arguments["apiBaseURL"] as? String?
+            val defaultUser = arguments["defaultUser"] as? String?
+            val debug = arguments["debug"] as? Boolean
+            val testMode = arguments["testMode"] as? Boolean
+
+
+            // Initialize the client
+            // Do not activate the lifecycle provider
+            val builder = TelemetryManager.Builder()
+                .appID(appID)
+                .providers(listOf(SessionProvider(), EnvironmentMetadataProvider()))
+
+            apiBaseURL?.let {
+                builder.baseURL(it)
+            }
+            defaultUser?.let {
+                builder.defaultUser(it)
+            }
+            debug?.let {
+                builder.showDebugLogs(it)
+            }
+            testMode?.let {
+                builder.testMode(it)
+            }
+
+            val application = applicationContext as Application
+            TelemetryManager.start(application, builder)
+            result.success(null)
+        } else {
+            result.error("INVALID_ARGUMENT", "Arguments are not a map", null)
+        }
+    }
+
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
 }
