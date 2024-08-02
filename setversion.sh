eddc37a79d5450f5cb14382be83ba5dcbb271045
Konstantin <konstantin@headbright.be>
2024-08-02 13:40:16 +0200

    fix: setversion.sh updating versions.dart with the current version from version.txt

--- setversion.sh
+++ setversion.sh
@@ -7,11 +7,11 @@ echo "Updating package versions to ${NEW_VERSION}."
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
   # macOS requires an empty string argument after -i to edit in place without backup
-  sed -i '' "s/^const String telemetryClientVersion = .*$/const String telemetryClientVersion = \"${NEW_VERSION}\";/g" "lib/versions.dart"
+  sed -i '' "s/^const String telemetryClientVersion = .*$/const String telemetryClientVersion = \"${NEW_VERSION}\";/g" "lib/src/versions.dart"
   sed -i '' "s/^version: .*/version: ${NEW_VERSION}/g" "pubspec.yaml"
 else
   # Linux
-  sed -i "/const String telemetryClientVersion =/c\const String telemetryClientVersion = \"${NEW_VERSION}\";" "lib/versions.dart"
+  sed -i "/const String telemetryClientVersion =/c\const String telemetryClientVersion = \"${NEW_VERSION}\";" "lib/src/versions.dart"
   sed -i "s/^version: .*/version: ${NEW_VERSION}/g" "pubspec.yaml"
 fi
 
